469a82c6d48bc0ae6c74986a71fb8d990c749943
yuyunaginagi <centrepg8@gmail.com>
2024-02-15 16:01:47 +0900

    third commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -35,7 +35,7 @@ class PrototypesController < ApplicationController
   def update
     @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to root_path
+      redirect_to prototype_path(@prototype)
     else
       render :edit, status: :unprocessable_entity
     end
